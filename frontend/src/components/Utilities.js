cf991da90d5b067acc7077c85fd248b3100285a9
susmita022 <85818236+susmita022@users.noreply.github.com>
2024-03-28 17:00:25 +0530

    Add files via upload

---
+++ frontend/src/components/Utilities.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CardItem from "./UtilityCard";
+
+function Utilities() {
+  return (
+    <div className="cards">
+      <h1>SUZUKI</h1>
+    </div>
+  );
+}
+
+export default Utilities;
---
+++ frontend/src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../App.css";
+import HeroSection from "../HeroSection";
+import Utilities from "../Utilities";
+import Footer from "../Footer";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Utilities />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
