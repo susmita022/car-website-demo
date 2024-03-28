c7d380bcf9eb1356fecebf681b08e9f863af137d
susmita022 <85818236+susmita022@users.noreply.github.com>
2024-03-28 12:56:59 +0530

    Add files via upload

---
+++ src/components/HeroSection.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <video src="/videos/banner.mp4" autoPlay loop muted />
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          GET STARTED
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
---
+++ src/components/Navbar.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            <img
+              src="images/maruti-logo.png"
+              alt="Maruti Logo"
+              className="logo-image"
+            />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link
+                to="/Services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Explore
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Contact Us
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                {button && <Button buttonStyle="btn--outline">Sign Up</Button>}
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/log-in"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                {button && <Button buttonStyle="btn--outline">Log in</Button>}
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
