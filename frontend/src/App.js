4e7fdee67b0424a0eca597616dcfc64f867cd80c
susmita022 <85818236+susmita022@users.noreply.github.com>
2024-04-09 11:19:50 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Home from "./components/pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import BookShowRoom from "./components/pages/BookShowRoom";
+import SignUp from "./components/pages/SignUp";
+import ContactUs from "./components/pages/ContactUs";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={Products} />
+          <Route path="/bookShowroom" component={BookShowRoom} />
+          <Route path="/sign-up" component={SignUp} />
+          <Route path="/ContactUs" component={ContactUs} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/pages/BookShowRoom.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../App.css";
+import TestDriveForm from "../TestDriveForm";
+import Footer from "../Footer";
+
+function BookShowRoom() {
+  return (
+    <>
+      <TestDriveForm />
+      <Footer />
+    </>
+  );
+}
+
+export default BookShowRoom;
