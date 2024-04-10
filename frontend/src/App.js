4d43c2046ac1785f5231f413a703b4d265c5125d
tanmayasamantray <80944495+tanmayasamantray@users.noreply.github.com>
2024-04-10 09:53:32 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,27 +8,7 @@ import Products from "./components/pages/Products";
 import BookShowRoom from "./components/pages/BookShowRoom";
 import SignUp from "./components/pages/SignUp";
 import ContactUs from "./components/pages/ContactUs";
-//bugs start
-let myObject = {
-    prop1: 'value1',
-    prop2: 'value2',
-    prop3: 'value3'
-};
 
-// Incorrect usage of 'in' with an object
-for (let key in myObject) {
-    console.log(key + ': ' + myObject[key]);
-}
-let result = 10 / "hello";
-
-if (result === NaN) {
-    console.log("Result is NaN");
-} else {
-    console.log("Result is not NaN");
-}
-let symbol = new Symbol('foo');
-
-//bugs end
 function App() {
   return (
     <>
