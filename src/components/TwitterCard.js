a55f3a4254061805d87f8f9e9994feb7b5c77874
susmita022 <85818236+susmita022@users.noreply.github.com>
2024-04-22 09:54:22 +0530

    Add files via upload

---
+++ src/components/TwitterCard.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function TwitterCard(props) {
+  return (
+    <>
+      <li className="cards__item">
+        <div className="card-social">
+          <a href={props.href} className="card-link">
+            <div className="card-heading">
+              <img src={props.src} alt="symbol" className="cards__itemm__img" />
+              <div>
+                {" "}
+                <h3>{props.heading}</h3>
+                <p className="hashtag">{props.hashtag}</p>
+              </div>
+            </div>
+            <div className="card-content">
+              <p>
+                {props.content} <br />
+                <span className="hashtag">{props.hashtag2}</span>
+              </p>
+            </div>
+          </a>
+        </div>
+      </li>
+    </>
+  );
+}
+
+export default TwitterCard;
---
+++ src/components/pages/GV.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../App.css";
+import GVpg from "../GVpg";
+import Footer from "../Footer";
+
+function GV() {
+  return (
+    <>
+      <GVpg />
+      <Footer />
+    </>
+  );
+}
+
+export default GV;
