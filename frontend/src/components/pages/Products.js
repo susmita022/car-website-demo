31ebdfdf6387be4a34ba28b253e7e4aca6e5ca28
susmita022 <85818236+susmita022@users.noreply.github.com>
2024-04-22 09:49:17 +0530

    Delete frontend/src directory

--- frontend/src/components/pages/Products.js
+++
@@ -1,9 +0,0 @@
-// frontend/src/components/pages/Product.js
-
-
-import React from "react";
-import "../../App.css";
-
-export default function Products() {
-  return <h1 className="products">PRODUCTS</h1>;
-}
